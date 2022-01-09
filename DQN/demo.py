aeb45b170d2d095dbd2dd6de9836fee4ce687ce6
MissSirius <2267408610@qq.com>
2022-01-09 12:05:38 +0800

    DQN V1.6 MPL4-32 BEST

--- DQN/demo.py
+++ DQN/demo.py
@@ -5,31 +5,12 @@ import argparse
 import torch
 import cv2
 from src.tetris import Tetris
-from src.deep_q_network import DQN256, DQN64
+from src.deep_q_network import MLP
 import random
 
 import numpy as np
 
-def setup_seed(seed):
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed_all(seed)
-    np.random.seed(seed)
-    random.seed(seed)
-    torch.backends.cudnn.deterministic = True
-
-def get_args():
-    parser = argparse.ArgumentParser(
-        """Implementation of Deep Q Network to play Tetris""")
-
-    parser.add_argument("--width", type=int, default=10, help="The common width for all images")
-    parser.add_argument("--height", type=int, default=20, help="The common height for all images")
-    parser.add_argument("--block_size", type=int, default=30, help="Size of a block")
-    parser.add_argument("--fps", type=int, default=300, help="frames per second")
-    parser.add_argument("--saved_path", type=str, default="trained_models128")
-    parser.add_argument("--output", type=str, default="output.mp4")
-
-    args = parser.parse_args()
-    return args
+from src.arg_parser import get_args
 
 
 def test(opt):
@@ -46,8 +27,6 @@ def test(opt):
     env.reset()
     if torch.cuda.is_available():
         model.cuda()
-    out = cv2.VideoWriter(opt.output, cv2.VideoWriter_fourcc(*'mp4v'), opt.fps,
-                          (int(1.5 * opt.width * opt.block_size), opt.height * opt.block_size))
     while True:
         next_steps = env.get_next_states()
         next_actions, next_states = zip(*next_steps.items())
@@ -57,10 +36,9 @@ def test(opt):
         predictions = model(next_states)[:, 0]
         index = torch.argmax(predictions).item()
         action = next_actions[index]
-        _, done = env.step(action, render=True, video=out)
+        _, done = env.step(action, render=True)
 
         if done:
-            out.release()
             final_score = env.score
             final_tetrominoes = env.tetrominoes
             final_cleared_lines = env.cleared_lines
@@ -73,5 +51,5 @@ def test(opt):
 
 
 if __name__ == "__main__":
-    opt = get_args()
+    opt = get_args(train=False)
     temp = test(opt)
